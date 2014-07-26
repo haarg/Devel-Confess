28396d5b256a0eb6130ab4ea0a88281d0dd207fe
Graham Knop <haarg@haarg.org>
2014-07-26 16:32:12 -0400

    bump version

--- lib/Devel/Confess.pm
+++ lib/Devel/Confess.pm
@@ -3,7 +3,7 @@ use 5.006;
 use strict;
 use warnings FATAL => 'all';
 
-our $VERSION = '0.007002';
+our $VERSION = '0.007003';
 $VERSION = eval $VERSION;
 
 use Carp ();
--- lib/Devel/Confess/Builtin.pm
+++ lib/Devel/Confess/Builtin.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings FATAL => 'all';
 no warnings 'once';
 
-our $VERSION = '0.007002';
+our $VERSION = '0.007003';
 $VERSION = eval $VERSION;
 
 {
