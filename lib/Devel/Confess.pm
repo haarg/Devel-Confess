500e65c260baa110efdd9261acc14c738b6f0b48
Graham Knop <haarg@haarg.org>
2015-06-26 02:53:20 -0400

    Bumping version to 0.008000

--- lib/Devel/Confess.pm
+++ lib/Devel/Confess.pm
@@ -16,7 +16,7 @@ use strict;
 use warnings;
 no warnings 'once';
 
-our $VERSION = '0.007012';
+our $VERSION = '0.008000';
 $VERSION = eval $VERSION;
 
 use Carp ();
--- lib/Devel/Confess/Builtin.pm
+++ lib/Devel/Confess/Builtin.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings FATAL => 'all';
 no warnings 'once';
 
-our $VERSION = '0.007012';
+our $VERSION = '0.008000';
 $VERSION = eval $VERSION;
 
 use Devel::Confess::_Util ();
