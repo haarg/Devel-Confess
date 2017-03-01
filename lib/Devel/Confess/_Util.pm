f986919a85597d394f8bb3db17f83afee48729a4
Graham Knop <haarg@haarg.org>
2017-03-01 17:28:16 +0100

    base util alternates on perl version

--- lib/Devel/Confess/_Util.pm
+++ lib/Devel/Confess/_Util.pm
@@ -30,39 +30,50 @@ sub weaken ($);
   : sub ($) { 0 };
 
 sub longmess;
-*longmess = !Carp->VERSION ? eval q{
-  package
-    Carp;
-  our (%CarpInternal, %Internal, $CarpLevel);
-  $CarpInternal{Carp}++;
-  $CarpInternal{warnings}++;
-  $Internal{Exporter}++;
-  $Internal{'Exporter::Heavy'}++;
-  sub {
-    my $level = 0;
-    while (1) {
-      my $p = (caller($level))[0] || last;
-      last
-        unless $CarpInternal{$p} || $Internal{$p};
-      $level++;
+if (!Carp->VERSION) {
+  eval q{
+    package
+      Carp;
+    our (%CarpInternal, %Internal, $CarpLevel);
+    $CarpInternal{'Carp'} ||= 1;
+    $CarpInternal{'warnings'} ||= 1;
+    $Internal{'Exporter'} ||= 1;
+    $Internal{'Exporter::Heavy'} ||= 1;
+
+    sub Devel::Confess::_Util::longmess {
+      my $level = 0;
+      while (1) {
+        my $p = (caller($level))[0] || last;
+        last
+          unless $CarpInternal{$p} || $Internal{$p};
+        $level++;
+      }
+      local $CarpLevel = $CarpLevel + $level;
+      no strict 'refs';
+      local *{"threads::tid"} = \&threads::tid
+        if defined &threads::tid && !defined &{"threads::tid"};
+      &longmess;
     }
-    local $CarpLevel = $CarpLevel + $level;
-    no strict 'refs';
-    local *{"threads::tid"} = \&threads::tid
-      if defined &threads::tid && !defined &{"threads::tid"};
-    &longmess;
-  };
-} : eval q{
-  package
-    Carp;
-  sub {
-    local $INC{'Carp/Heavy.pm'} = $INC{'Carp/Heavy.pm'} || 1;
-    no strict 'refs';
-    local *{"threads::tid"} = \&threads::tid
-      if defined &threads::tid && !defined &{"threads::tid"};
-    &longmess;
-  };
-} or die $@;
+
+    1;
+  } or die $@;
+}
+else {
+  eval q{
+    package
+      Carp;
+
+    sub Devel::Confess::_Util::longmess {
+      local $INC{'Carp/Heavy.pm'} = $INC{'Carp/Heavy.pm'} || 1;
+      no strict 'refs';
+      local *{"threads::tid"} = \&threads::tid
+        if defined &threads::tid && !defined &{"threads::tid"};
+      &longmess;
+    }
+
+    1;
+  } or die $@;
+}
 
 if (defined &Carp::format_arg && $Carp::VERSION < 1.32) {
   my $format_arg = \&Carp::format_arg;
@@ -70,7 +81,7 @@ if (defined &Carp::format_arg && $Carp::VERSION < 1.32) {
     package
       Carp;
     our $in_recurse;
-    $format_arg if 0; # capture
+    $format_arg if 0; # capture for 5.6
     no warnings 'redefine';
     sub format_arg {
       if (! $in_recurse) {
@@ -93,85 +104,94 @@ if (defined &Carp::format_arg && $Carp::VERSION < 1.32) {
           return $arg;
         }
       }
-      $format_arg->(@_);
+      &$format_arg;
     }
+
     1;
   } or die $@;
 }
 
 sub _str_val;
-eval q{
-  sub _str_val {
-    no overloading;
-    "$_[0]";
-  }
-  1;
-} or eval q{
-  sub _str_val {
-    my $class = &blessed;
-    return "$_[0]" unless defined $class;
-    return sprintf("%s=%s(0x%x)", $class, &reftype, &refaddr);
-  }
-  1;
-} or die $@;
+if ("$]" >= 5.010_001) {
+  eval q{
+    sub _str_val {
+      no overloading;
+      "$_[0]";
+    }
+
+    1;
+  } or die $@;
+}
+else {
+  eval q{
+    sub _str_val {
+      my $class = &blessed;
+      return "$_[0]" unless defined $class;
+      return sprintf("%s=%s(0x%x)", $class, &reftype, &refaddr);
+    }
+
+    1;
+  } or die $@;
+}
 
 sub _global_destruction ();
 sub _in_END ();
-{
-  if (defined ${^GLOBAL_PHASE}) {
-    eval q{
-      sub _global_destruction () { ${^GLOBAL_PHASE} eq q[DESTRUCT] }
-      sub _in_END () { ${^GLOBAL_PHASE} eq "END" }
-      1;
-    } or die $@;
-  }
-  else {
-    eval q{
-      # this is slightly a lie, but accurate enough for our purposes
-      our $global_phase = 'RUN';
+if ("$]" >= 5.014_000) {
+  eval q{
+    sub _global_destruction () { ${^GLOBAL_PHASE} eq "DESTRUCT" }
+    sub _in_END             () { ${^GLOBAL_PHASE} eq "END" }
 
-      sub _global_destruction () {
-        if ($global_phase ne 'DESTRUCT') {
-          local $SIG{__WARN__} = sub {
-            $global_phase = 'DESTRUCT' if $_[0] =~ /global destruction\.\n\z/
-          };
-          warn 1;
-        }
-        $global_phase eq 'DESTRUCT';
+    1;
+  } or die $@;
+}
+else {
+  eval q{
+    # this is slightly a lie, but accurate enough for our purposes
+    our $global_phase = 'RUN';
+
+    sub _global_destruction () {
+      if ($global_phase ne 'DESTRUCT') {
+        local $SIG{__WARN__} = sub {
+          $global_phase = 'DESTRUCT'
+            if $_[0] =~ /global destruction\.\n\z/;
+        };
+        warn 1;
       }
+      $global_phase eq 'DESTRUCT';
+    }
 
-      sub _in_END () {
-        if ($global_phase eq 'RUN' && $^S) {
-          # END blocks are FILO so we can't install one to run first.
-          # only way to detect END reliably seems to be by using caller.
-          # I hate this but it seems to be the best available option.
-          # The top two frames will be an eval and the END block.
-          my $i;
-          1 while CORE::caller(++$i);
-          if ($i > 2) {
-            my @top = CORE::caller($i - 1);
-            my @next = CORE::caller($i - 2);
-            if (
-              $top[3] eq '(eval)'
-              && $next[3] =~ /::END$/
-              && $top[2] == $next[2]
-              && $top[1] eq $next[1]
-              && $top[0] eq 'main'
-              && $next[0] eq 'main'
-            ) {
-              $global_phase = 'END';
-            }
+    sub _in_END () {
+      if ($global_phase eq 'RUN' && $^S) {
+        # END blocks are FILO so we can't install one to run first.
+        # only way to detect END reliably seems to be by using caller.
+        # I hate this but it seems to be the best available option.
+        # The top two frames will be an eval and the END block.
+        my $i;
+        1 while CORE::caller(++$i);
+        if ($i > 2) {
+          my @top = CORE::caller($i - 1);
+          my @next = CORE::caller($i - 2);
+          if (
+            $top[3] eq '(eval)'
+            && $next[3] =~ /::END$/
+            && $top[2] == $next[2]
+            && $top[1] eq $next[1]
+            && $top[0] eq 'main'
+            && $next[0] eq 'main'
+          ) {
+            $global_phase = 'END';
           }
         }
-        $global_phase eq 'END';
       }
-      END {
-        $global_phase = 'END';
-      }
+      $global_phase eq 'END';
+    }
 
-      1;
-    } or die $@;
-  }
+    END {
+      $global_phase = 'END';
+    }
+
+    1;
+  } or die $@;
 }
 
 sub _can_stringify ();
@@ -189,6 +209,7 @@ if ("$]" < 5.008) {
       }
       return 1;
     }
+
     1;
   } or die $@;
 }
@@ -197,6 +218,7 @@ else {
     sub _can_stringify () {
       defined $^S && !$^S;
     }
+
     1;
   } or die $@;
 }
